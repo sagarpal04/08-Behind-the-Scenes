dad15e173fa0ca1f54b1563c2ec603ca358e5c0e
sagarpal04 <sagarpal0402@gmail.com>
2024-03-12 13:37:23 +0530

    Done

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 'use strict';
-function calcAge(birthYear) {
-  const age = 2037 - birthYear;
-  function printAge() {
-    const output = `${firstName}, you are ${age}, born in ${birthYear}`;
-    console.log(output);
-    const sagar = 1;
-    if (birthYear >= 1981 && birthYear <= 1996) {
-      var millenial = true;
-      const sagar = 2;
-      const str = `Oh, and you're a millenial, ${firstName}`;
-      console.log(str);
-    }
-    console.log(millenial);
-  }
-  printAge();
-}
-const firstName = 'Jonas';
-calcAge(1991);
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age); // 31
+console.log(oldAge); // 30
+
+const me = {
+  name: 'Jonas',
+  age: 30,
+};
+
+const friend = me;
+friend.age = 31;
+console.log(me);
+console.log(friend);
